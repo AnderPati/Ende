a4f8c0807d73fa4bcbadb528a762830853c5c7ea
Ander P <Ander P@DESKTOP-97UPHP1>
2020-11-18 13:45:26 +0100

    menú 4 opciones

---
+++ src/menuakGit/menuak.java
@@ -0,0 +1,13 @@
+package menuakGit;
+
+public class menuak {
+
+	public static void main(String[] args) {
+
+		System.out.println("Opcion 1\r"
+				+ "Opcion 2\r"
+				+ "Opcion 3\r"
+				+ "Opcione 4");
+	}
+
+}
