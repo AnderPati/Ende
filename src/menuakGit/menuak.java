fce10e5954dc9abf7ff5a49edc03f0f29ec5fd91
Ander P <Ander P@DESKTOP-97UPHP1>
2020-11-18 13:59:06 +0100

    p1

--- src/menuakGit/menuak.java
+++ src/menuakGit/menuak.java
@@ -1,13 +1,22 @@
+import java.util.Scanner;
+
 package menuakGit;
 
 public class menuak {
 
 	public static void main(String[] args) {
-
+		Scanner sc=new Scanner(System.in);
+		
+		int i;
+		
 		System.out.println("Opcion 1\r"
 				+ "Opcion 2\r"
 				+ "Opcion 3\r"
-				+ "Opciones 4");
+				+ "Opcion 4");
+		
+		switch(i) {
+		
+		}
 	}
 
 }
